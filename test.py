f24bc48fc399c33086b2cf3a6096a39fe69604eb
julian-patterson <julianedwardpatterson@gmail.com>
2022-04-16 22:26:34 -0400

    fade_rainbow test

--- test.py
+++ test.py
@@ -2,32 +2,31 @@ import asyncio
 import kasa
 import time
 
+bulb = kasa.SmartBulb('10.0.0.87')
+bulb2 = kasa.SmartBulb('10.0.0.37')
+
 async def rainbow():
-    bulb = kasa.SmartBulb('10.0.0.87')
-    bulb2 = kasa.SmartBulb('10.0.0.37')
+    while True:
+        for i in range(359):
+            await bulb.update()
+            await bulb2.update()
+            await bulb.set_hsv(i, 100, 100,  transition= 50)
+            await bulb2.set_hsv(i, 100, 100, transition= 50)
+
+
+async def fade_rainbow():
     while True:
         for i in range(359):
             await bulb.update()
             await bulb2.update()
             await bulb.set_hsv(i, 100, 100)
             await bulb2.set_hsv(i, 100, 100)
-            await bulb.update()
-            await bulb2.update()
-            time.sleep(1/1000)
+            await bulb.turn_off(transition= 1000)
+            await bulb2.turn_off(transition= 1000)
+            i += 10
 
-
-            
-
-async def main():
-    bulb = kasa.SmartBulb('10.0.0.87')
-    bulb2 = kasa.SmartBulb('10.0.0.37')
-    while True:
-        await bulb.update()
-        await bulb2.update()
-        await bulb.set_hsv(180, 100, 88)
-        await bulb2.set_hsv(180, 100, 88)
-        time.sleep()
-
+        
 
 if __name__ == '__main__':
-    asyncio.run(rainbow())
+    #asyncio.run(rainbow())
+    asyncio.run(fade_rainbow())
